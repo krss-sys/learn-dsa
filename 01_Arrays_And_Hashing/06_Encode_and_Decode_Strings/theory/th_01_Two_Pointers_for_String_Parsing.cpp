36fe4b39c734f50f62ed725e71e61fad1352c537
krss-sys <kgm.naoya@gmail.com>
2026-08-07 18:14:42 +0700

    docs: add EN-JP speech scripts for string parsing practice

--- 01_Arrays_And_Hashing/06_Encode_and_Decode_Strings/theory/th_01_Two_Pointers_for_String_Parsing.cpp
+++ 01_Arrays_And_Hashing/06_Encode_and_Decode_Strings/theory/th_01_Two_Pointers_for_String_Parsing.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    string data = "apple#banana#cherry#date";  // chuoi mau
+    string data = "apple#banana#cherry#date";  
     int i = 0;
     int n = data.size();
 
@@ -12,15 +12,21 @@ int main() {
     while (i < n) {
         int j = i;
 
-        // Tim vi tri dau # hoac het chuoi
+        //ENG: I find the next hash sign.
+        //JPN: 次の「#(hashu)」を探します。
+        //VIE: Tôi tìm dấu # tiếp theo
         while (j < n && data[j] != '#') {
             j++;
         }
 
-        // In ra phan tu tu i den j - 1
+        //ENG: I get the current word and print it.
+        //JPN: 現在の単語を取得して表示します。
+        //VIE: Tôi lấy từ hiện tại và in ra.
         cout << "- " << data.substr(i, j - i) << endl;
 
-        // Nhay den vi tri sau #
+        //ENG: I move to the next word.
+        //JPN: 次の単語に移動します。
+        //VIE: Tôi chuyển sang từ tiếp theo.
         i = j + 1;
     }
 
--- 01_Arrays_And_Hashing/06_Encode_and_Decode_Strings/theory/th_02_String_Substring_Extraction_and_Conversion.cpp
+++ 01_Arrays_And_Hashing/06_Encode_and_Decode_Strings/theory/th_02_String_Substring_Extraction_and_Conversion.cpp
@@ -14,16 +14,27 @@ int main() {
             j++;
         }
 
+        //ENG: I convert the string to an integer.
+        //JPN: 文字列を数値に変換します。
+        //VIE: Tôi đổi chuỗi thành số.
         string numStr = data.substr(i, j - i);
         int number = stoi(numStr);
 
         cout << "So: " << number << "\n";
+
+        //ENG: I add the number to sum.
+        //JPN: 数字を合計に足します。
+        //VIE: Tôi cộng số này vào tổng.
         sum += number;
 
         i = j + 1;
     }
 
     cout << "Tong cac so: " << sum << "\n";
+
+    //ENG: I make a message string from the sum.
+    //JPN: 合計からメッセージを作ります。
+    //VIE: Tôi tạo thông báo từ kết quả tổng.
     string message = "Ket qua tong: " + to_string(sum);
     cout << message << "\n";
 
